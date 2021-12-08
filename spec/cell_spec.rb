c2fb37f960aa967c24c0d6f3f3bc5cd861f6108a
Lisa Taylor <ltaylor.lisa@gmail.com>
2021-12-08 12:25:27 -0700

    Feat: cell coordinates work

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,9 +3,13 @@ require './lib/ship'
 require './lib/cell'
 
 RSpec.describe Cell do
-
   it 'is a cell' do
     cell = Cell.new("B4")
     expect(cell).to be_instance_of(Cell)
+  end
+
+  it 'coordinate equals B4' do
+    cell = Cell.new("B4")
+    expect(cell.coordinate).to eq("B4")
   end
 end
