ebde3fc898422f3c1b3af10d148830e07f7b0486
benistta <92293363+benistta@users.noreply.github.com>
2021-12-09 19:55:55 -0700

    Fix: fired_upon, render

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,14 +1,18 @@
 class Cell
-  attr_reader :coordinate, :ship, :render
+  attr_reader :coordinate, :ship, :fired_upon #render
 
-  def initialize(coordinate)
+  def initialize(coordinate)#render = ".")
     @coordinate = coordinate
     @ship = nil
-    @render = "."
+    #@render = render
+    @fired_upon = false
   end
 
   def place_ship(picked_ship)
     @ship = picked_ship
+    #if @render == true
+    #@render = "S"
+    #end
   end
 
   def empty?
@@ -19,23 +23,50 @@ class Cell
     end
   end
 
+  def fired_upon?
+    @fired_upon
+  end
+
   def fire_upon
-    if @ship == nil
-      @render = "M"
-    elsif @ship != nil
-      @render = "H"
-      @ship.hit
-    elsif @ship.sunk? == true
-      @render = "X"
+    @ship.hit if empty? == false
+  @fired_upon = true
+  end
+
+  def render(arg = false)
+    if @ship != nil && @ship.sunk?
+    "X"
+    elsif fired_upon? && @ship != nil
+    "H"
+    elsif fired_upon? == true
+    "M"
+    else
+    "."
     end
   end
 
+#  def fire_upon  # our "." and "S" can be included in this if statemnet(conditional), google how to add an optional argument
+#    if @ship == nil # remeber we dont have an instance variable for render anymore.
+#      @render = "M"
+#    elsif @ship != nil
+#      @render = "H"
+#      @ship.hit
+#    elsif @ship.sunk? == true
+#      @render = "X"
+#    end
+#  end
+
+#fire upon method know doesnt exist and needs to be created. /zzzzzzzz
+
 #### this method may be questionable?
-  def fired_upon?
-    if @ship.health < @ship.length
-      true
-    else
-      false
-    end
-  end
+  #def fired_upon?
+  #  if @ship.health < @ship.length
+  #    true
+  #  else
+  #    false
+  #  end
+  #end
+
+  #def render(true)
+  #  @render = "S"
+  #end
 end
