2ad0b6790f5caddf6967244c928d36d2a3bcece0
Lisa Taylor <ltaylor.lisa@gmail.com>
2021-12-09 21:36:09 -0700

    Feat: valid placement for ships is false as tested

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -28,12 +28,13 @@ RSpec.describe Board do
     expect(board.valid_coordinate?("A22")).to eq(false)
   end
 
-  it 'valid coordinate passes' do
+  it 'valid placement for ships is false as tested' do
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
 
     expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
   end
 
 
