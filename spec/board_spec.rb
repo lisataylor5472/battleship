6ce570e5da5dcdd6f7faf2a8ef30fdf4533bca64
Lisa Taylor <ltaylor.lisa@gmail.com>
2021-12-09 21:12:25 -0700

    Feat: cells creates 16 cells

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,6 +1,6 @@
 require 'pry'
-# require './lib/ship'
-# require './lib/cell'
+require './lib/ship'
+require './lib/cell'
 require './lib/board'
 
 RSpec.describe Board do
@@ -9,4 +9,15 @@ RSpec.describe Board do
 
     expect(board).to be_instance_of(Board)
   end
+
+  it 'cells - creates 16 cells' do
+    board = Board.new
+
+    expect(board.cells.keys.count).to eq(16)
+    expect(board.cells).to be_a(Hash)
+    expect(board.cells.values[0]).to be_a(Cell)
+
+  end
+
+
 end
