42bcb3d0d4c360c2e97c502ff124d002e8360213
benistta <92293363+benistta@users.noreply.github.com>
2021-12-09 20:14:00 -0700

    Feat: passed all tests

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -126,5 +126,24 @@ RSpec.describe Cell do
     cell_2.place_ship(cruiser)
     #cell_1.fire_upon
     expect(cell_2.render).to eq("H")
+    expect(cruiser.sunk?).to eq(false)
+  end
+
+  it 'render cell_2 to eq "S"' do
+    #cell = Cell.new("B4")
+    #cruiser = Ship.new("Cruiser", 3)
+    #cell.place_ship(cruiser)
+    #cell.fire_upon
+    cell_1 = Cell.new("B4")
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_1.fire_upon
+    cell_2.place_ship(cruiser)
+    cell_2.fire_upon
+    cruiser.hit
+    cruiser.hit
+
+    expect(cruiser.sunk?).to eq(true)
+    expect(cell_2.render).to eq("X")
   end
 end
