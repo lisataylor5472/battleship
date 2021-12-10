b3a536d4717b6dd5e04dfc285d2965da512fab95
Lisa Taylor <ltaylor.lisa@gmail.com>
2021-12-09 20:41:31 -0700

    Feat: board exists

---
+++ spec/board_spec.rb
@@ -0,0 +1,12 @@
+require 'pry'
+# require './lib/ship'
+# require './lib/cell'
+require './lib/board'
+
+RSpec.describe Board do
+  it 'exists' do
+    board = Board.new
+
+    expect(board).to be_instance_of(Board)
+  end
+end
