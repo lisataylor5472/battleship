3f534726f46a7928115582774262d99b5343ee58
benistta <92293363+benistta@users.noreply.github.com>
2021-12-09 20:19:23 -0700

    Fix: refactor cell and cell spec

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,18 +1,14 @@
 class Cell
-  attr_reader :coordinate, :ship, :fired_upon #render
+  attr_reader :coordinate, :ship, :fired_upon
 
-  def initialize(coordinate)#render = ".")
+  def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
-    #@render = render
     @fired_upon = false
   end
 
   def place_ship(picked_ship)
     @ship = picked_ship
-    #if @render == true
-    #@render = "S"
-    #end
   end
 
   def empty?
@@ -45,30 +41,4 @@ class Cell
     "."
     end
   end
-
-#  def fire_upon  # our "." and "S" can be included in this if statemnet(conditional), google how to add an optional argument
-#    if @ship == nil # remeber we dont have an instance variable for render anymore.
-#      @render = "M"
-#    elsif @ship != nil
-#      @render = "H"
-#      @ship.hit
-#    elsif @ship.sunk? == true
-#      @render = "X"
-#    end
-#  end
-
-#fire upon method know doesnt exist and needs to be created. /zzzzzzzz
-
-#### this method may be questionable?
-  #def fired_upon?
-  #  if @ship.health < @ship.length
-  #    true
-  #  else
-  #    false
-  #  end
-  #end
-
-  #def render(true)
-  #  @render = "S"
-  #end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -5,21 +5,25 @@ require './lib/cell'
 RSpec.describe Cell do
   it 'is a cell' do
     cell = Cell.new("B4")
+
     expect(cell).to be_instance_of(Cell)
   end
 
   it 'coordinate equals B4' do
     cell = Cell.new("B4")
+
     expect(cell.coordinate).to eq("B4")
   end
 
   it 'ship equals nil' do
     cell = Cell.new("B4")
+
     expect(cell.ship).to eq(nil)
   end
 
   it 'cell is empty' do
     cell = Cell.new("B4")
+
     expect(cell.empty?).to eq(true)
   end
 
@@ -27,6 +31,7 @@ RSpec.describe Cell do
     cell = Cell.new("B4")
     cruiser = Ship.new("Cruiser", 3)
     cell.place_ship(cruiser)
+
     expect(cell.ship).to eq(cruiser)
   end
 
@@ -34,6 +39,7 @@ RSpec.describe Cell do
     cell = Cell.new("B4")
     cruiser = Ship.new("Cruiser", 3)
     cell.place_ship(cruiser)
+
     expect(cell.empty?).to eq(false)
   end
 
@@ -64,76 +70,53 @@ RSpec.describe Cell do
   end
 
   it 'render "."' do
-    #cell = Cell.new("B4")
-    #cruiser = Ship.new("Cruiser", 3)
-    #cell.place_ship(cruiser)
-    #cell.fire_upon
     cell_1 = Cell.new("B4")
+
     expect(cell_1.render).to eq(".")
-    #cell_1.fire_upon
+
 
   end
 
   it 'render "M"' do
-    #cell = Cell.new("B4")
-    #cruiser = Ship.new("Cruiser", 3)
-    #cell.place_ship(cruiser)
-    #cell.fire_upon
     cell_1 = Cell.new("B4")
     cell_1.fire_upon
-    #cell_1.fire_upon
+
     expect(cell_1.render).to eq("M")
   end
 
   it 'render cell_2 to eq "."' do
-    #cell = Cell.new("B4")
-    #cruiser = Ship.new("Cruiser", 3)
-    #cell.place_ship(cruiser)
-    #cell.fire_upon
     cell_1 = Cell.new("B4")
     cell_2 = Cell.new("C3")
     cruiser = Ship.new("Cruiser", 3)
     cell_1.fire_upon
     cell_2.place_ship(cruiser)
-    #cell_1.fire_upon
+
     expect(cell_2.render).to eq(".")
   end
 
-  it 'render cell_2 to eq "S"' do
-    #cell = Cell.new("B4")
-    #cruiser = Ship.new("Cruiser", 3)
-    #cell.place_ship(cruiser)
-    #cell.fire_upon
+  it 'render cell_2 to true"' do
     cell_1 = Cell.new("B4")
     cell_2 = Cell.new("C3")
     cruiser = Ship.new("Cruiser", 3)
     cell_1.fire_upon
     cell_2.place_ship(cruiser)
-    #cell_1.fire_upon
+
     expect(cell_2.render(true)).to eq("S")
   end
 
-  it 'render cell_2 to eq "S"' do
-    #cell = Cell.new("B4")
-    #cruiser = Ship.new("Cruiser", 3)
-    #cell.place_ship(cruiser)
-    #cell.fire_upon
+  it 'render cell_2 to eq "H", sunk is false' do
     cell_1 = Cell.new("B4")
     cell_2 = Cell.new("C3")
     cruiser = Ship.new("Cruiser", 3)
     cell_1.fire_upon
     cell_2.fire_upon
     cell_2.place_ship(cruiser)
-    #cell_1.fire_upon
+
     expect(cell_2.render).to eq("H")
     expect(cruiser.sunk?).to eq(false)
   end
 
-  it 'render cell_2 to eq "S"' do
-    #cell = Cell.new("B4")
-    #cruiser = Ship.new("Cruiser", 3)
-    #cell.place_ship(cruiser)
-    #cell.fire_upon
+  it 'render cell_2 to eq "X", sunk true' do
     cell_1 = Cell.new("B4")
     cell_2 = Cell.new("C3")
     cruiser = Ship.new("Cruiser", 3)
