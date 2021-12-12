964de6716db261ca6ac2f0a17b332242d8762253
Lisa Taylor <ltaylor.lisa@gmail.com>
2021-12-12 15:14:13 -0700

    Feat: Board render showing up in Battleship runner

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -10,3 +10,9 @@ cruiser = Ship.new("Cruiser", 3)
 submarine = Ship.new("Submarine", 2)
 
 game_1.start_or_quit(gets.chomp)
+puts "I have laid out my ships on the grid."
+puts "You now need to lay out your two ships"
+puts "The Cruiser is three units long and the Submarine is two units long."
+puts board.render
+puts "Enter the squares for the Cruiser (3 spaces):"
+game_1.place_ship(gets.chomp)
